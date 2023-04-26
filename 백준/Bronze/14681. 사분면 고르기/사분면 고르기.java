470b842c63d0271d0af6ebd02a0280a90e55880d
schdevv <110762380+schdevv@users.noreply.github.com>
2023-04-27 00:58:58 +0900

    [Bronze V] Title: 사분면 고르기, Time: 216 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int X = in.nextInt();
+		int Y = in.nextInt();
+        
+		if(X>0){
+            if(Y>0) System.out.println(1);
+            else if(Y<0) System.out.println(4);
+        }
+		else if(Y>0)System.out.println(2);
+        else System.out.println(3);
+    }
+}
