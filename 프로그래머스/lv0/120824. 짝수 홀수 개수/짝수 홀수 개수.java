860ab79d710bdf175e8e4cdbc46dd4a635c09862
schdevv <110762380+schdevv@users.noreply.github.com>
2023-05-09 10:07:24 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int a = 0;
+        int b = 0;
+            for(int i=0; i<num_list.length; i++){
+                if(num_list[i] % 2 == 0){
+                    a++;
+                } else {
+                    b++;
+                }
+            }
+        int[] answer = new int[]{a,b};
+        return answer;
+    }
+}
