7161d5e97f22471520e61e1e14d328e7ea0c1ddc
schdevv <110762380+schdevv@users.noreply.github.com>
2023-10-17 15:55:28 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int oddSum = 0, evenSum = 0; 
+        
+        for(int i = 0; i < num_list.length; i++){
+            if(i % 2 != 0){ 
+                oddSum += num_list[i];
+            } else { 
+                evenSum += num_list[i];
+            }
+        }
+        
+        return Math.max(oddSum, evenSum); 
+    }
+}
